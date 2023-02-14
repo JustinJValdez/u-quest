1de18bfa433964bddc5e947e31cdafbfdd6b9f56
JustinJValdez <justinjvaldez@gmail.com>
2023-02-14 12:52:49 -0700

    WriteCommit

---
+++ pages/comps/dice.js
@@ -0,0 +1,19 @@
+export default function Dice() {
+
+ function roll (){
+    alert("test");
+  }
+  
+  return (
+
+    <div>
+      DICE FEATURE
+      <div>
+        <button onClick={roll}>Roll Da Dice</button>
+      </div>
+    </div>
+
+  )
+  
+  
+};

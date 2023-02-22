28776a47886ed5bea1dcfb9aa431eaa08d03c785
JustinJValdez <justinjvaldez@gmail.com>
2023-02-21 21:46:37 -0700

    added random number on click for dice.js

--- pages/comps/dice.js
+++ pages/comps/dice.js
@@ -1,15 +1,18 @@
+import styles from "../../styles/Dice.module.css"
+
 export default function Dice() {
 
  function roll (){
-    alert("test");
+    var rnd = Math.floor(Math.random()* 20);
+    document.getElementById('dice').innerHTML = rnd;
   }
   
   return (
 
     <div>
       DICE FEATURE
-      <div>
-        <button onClick={roll}>Roll Da Dice</button>
+      <div className={styles.dice}>
+        <button id='dice' onClick={roll}>Roll Da Dice</button>
       </div>
     </div>
 
